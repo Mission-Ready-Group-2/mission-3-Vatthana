78efd69b9edf56a5967ce65a45a78ab47b88c1ec
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2023-11-25 13:03:35 +1300

    Update port number in server configuration

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const router = express.Router();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 1337;
 
 // CORS
 app.use(cors());
